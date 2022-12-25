cdaf1803ae2413192a4ce2b53c66cfbc19752f86
Harsh Dev <devh9933@gmail.com>
2022-12-25 17:10:06 +0530

    written print keyword combination code

--- Recursion 2/print_keyword_combinations_code.cpp
+++ Recursion 2/print_keyword_combinations_code.cpp
@@ -1,77 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int keypad(int num, string output[]){
-    if(num==0 || num==1){
-        output[0]="";
-        return 1;
+void printKeypad1(int num,string out){
+    if(num==1 || num==0){
+        cout<<out<<endl;
+        return;
     }
-    int t=keypad(num/10,output);
-    int b=num%10;
-    int l;
-    string k;
-    switch(b){
-        case 0:
-        l=1;
-        k="";
-        break;
-        case 1:
-        l=1;
-        k="";
-        break;
-        case 2:
-        l=3;
-        k="abc";
-        break;
-        case 3:
-        l=3;
-        k="def";
-        break;
-        case 4:
-        l=3;
-        k="ghi";
-        break;
-        case 5:
-        l=3;
-        k="jkl";
-        break;
-        case 6:
-        l=3;
-        k="mno";
-        break;
-        case 7:
-        l=4;
-        k="pqrs";
-        break;
-        case 8:
-        l=3;
-        k="tuv";
-        break;
-        case 9:
-        l=4;
-        k="wxyz";
-        break;
-        default:
-        break;
-    }
-    for(int i=0;i<t;i++){
-        string s=output[i];
-        for(int j=0;j<l;j++){
-            output[j*t+i]=s+k[j];
-        }
-    }
-    return l*t;
+    string arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    string s=arr[num%10];
+    for(int i=0;i<s.size();i++){
+        printKeypad1(num/10,s[i]+out);
+    } 
+}
+void printKeypad(int num){
+    string out;
+    printKeypad1(num,out);
 }
 int main(){
     int num;
     cin >> num;
-    string output[10000];
-    output[0]="";
-    int count = keypad(num, output);
-    cout<<count<<endl;
-    for(int i = 0; i < count && i < 10000; i++){
-        cout << output[i] << endl;
-    }
+    printKeypad(num);
     return 0;
 }
-
