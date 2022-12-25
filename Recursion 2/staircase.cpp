ae97f819daa9dce981e4d2890259a99daa03a315
Harsh Dev <devh9933@gmail.com>
2022-12-26 00:59:40 +0530

    Updated staircase.cpp

--- Recursion 2/staircase.cpp
+++ Recursion 2/staircase.cpp
@@ -2,26 +2,16 @@
 using namespace std;
 int staircase(int n)
 {
-    if (n == 0)
-    {
+    if(n==0){
         return 1;
     }
-    if (n == 1)
-    {
+    if(n==1){
         return 1;
     }
-    if (n == 2)
-    {
+    if(n==2){
         return 2;
     }
-    if (n == 3)
-    {
-        return 4;
-    }
-    int smalloutput1 = staircase(n - 1);
-    int smalloutput2 = staircase(n - 2);
-    int smalloutput3 = staircase(n - 3);
-    return smalloutput3 + smalloutput2 + smalloutput1;
+    return staircase(n-3)+staircase(n-2)+staircase(n-1);
 }
 int main()
 {
