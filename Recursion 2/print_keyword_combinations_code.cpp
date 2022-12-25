f91cbb42777930eedda65062582fc4fdc7d487f8
Harsh Dev <devh9933@gmail.com>
2022-12-25 14:55:08 +0530

    Deleted Exe Files and updated Print Keyword

--- Recursion 2/print_keyword_combinations_code.cpp
+++ Recursion 2/print_keyword_combinations_code.cpp
@@ -1,78 +1,77 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-void printKeypad1(int num,string output){
-    if(num==0 | num==1){
-        cout<<output<<endl;
-        return;
+int keypad(int num, string output[]){
+    if(num==0 || num==1){
+        output[0]="";
+        return 1;
     }
+    int t=keypad(num/10,output);
     int b=num%10;
-    int length;
+    int l;
     string k;
     switch(b){
         case 0:
-        length=1;
+        l=1;
         k="";
         break;
         case 1:
-        length=1;
+        l=1;
         k="";
         break;
         case 2:
-        length=3;
+        l=3;
         k="abc";
         break;
         case 3:
-        length=3;
+        l=3;
         k="def";
         break;
         case 4:
-        length=3;
+        l=3;
         k="ghi";
         break;
         case 5:
-        length=3;
+        l=3;
         k="jkl";
         break;
         case 6:
-        length=3;
+        l=3;
         k="mno";
         break;
         case 7:
-        length=4;
+        l=4;
         k="pqrs";
         break;
         case 8:
-        length=3;
+        l=3;
         k="tuv";
         break;
         case 9:
-        length=4;
+        l=4;
         k="wxyz";
         break;
         default:
         break;
     }
-    
-    for(int i=0;i<length;i++){
-        string extra=output;
-    for(int j=0;j<output.size();j++){
-        output[i +j*i]=extra+k;
-        printKeypad1(num/10, output);
+    for(int i=0;i<t;i++){
+        string s=output[i];
+        for(int j=0;j<l;j++){
+            output[j*t+i]=s+k[j];
+        }
     }
-    }
-    
-}
-void printKeypad(int num){
-    string output="";
-    printKeypad1(num, output);
+    return l*t;
 }
 int main(){
     int num;
     cin >> num;
-
-    printKeypad(num);
-
+    string output[10000];
+    output[0]="";
+    int count = keypad(num, output);
+    cout<<count<<endl;
+    for(int i = 0; i < count && i < 10000; i++){
+        cout << output[i] << endl;
+    }
     return 0;
 }
+
--- Recursion 2/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-int binarySearch(int input[], int length,int  element){
-    if(*input==element ){
-        return 0;
-    }
-    return 1 + binarySearch( input+1,  length-1,  element);
-}
