2b2add7122dfb4f62c7b5978d23911308b42138f
Harsh Dev <devh9933@gmail.com>
2022-12-25 15:26:28 +0530

    Added Approach 2 to print subsequences of string

--- Recursion 2/print_subsequences_of_string.cpp
+++ Recursion 2/print_subsequences_of_string.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+// Approach 1 
 void printsubsequences(string input, string output)
 {
     if (input.length() == 0)
@@ -10,6 +11,16 @@ void printsubsequences(string input, string output)
     }
     printsubsequences(input.substr(1), output);
     printsubsequences(input.substr(1), output + input[0]);
+}
+// Approach 2
+void printsubsequences(string input, string output)
+{
+    if(input.size()==0){
+        cout<<output<<endl;
+        return;
+    }
+    printsubsequences(input.substr(0,input.size()-1),output);
+    printsubsequences(input.substr(0,input.size()-1),input.substr(input.size()-1)+output);
 }
 int main()
 {
